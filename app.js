a7358d574da3c163c97c1e3fc3fa8d146d026783
Jacob Paul <jakepaul1992@gmail.com>
2021-03-18 16:46:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('hello world');
+let maximum = parseInt(prompt('Enter the maximum number!'));
+// set boundaries for integer
+while (!maximum) {
+	maximum = parseInt(prompt('Enter a valid number!'));
+}
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+// define guess and set parseInt boundary
+let guess = parseInt(prompt('Enter your first guess!'));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+	if (guess === 'q') break;
+	attempts++;
+	if (guess > targetNum) {
+		guess = prompt('Too high!  Enter a new guess:');
+	} else {
+		guess = prompt('Too low! enter a new guess:');
+	}
+}
+// allow quit option, console.log final statement after guessing correctly
+if (guess === 'q') {
+	console.log('Okay, game stopped!');
+} else {
+	console.log('Good job! You did it!');
+	console.log(`It took you ${attempts} guesses!`);
+}
